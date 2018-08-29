8cc69ef33e434ad6a8821cfa155c4e70c89577d0
Norm Matloff <matloff@cs.ucdavis.edu>
2018-08-29 10:22:58 -0700

    comments

--- R/polyanNA.R
+++ R/polyanNA.R
@@ -209,7 +209,6 @@ lm.pa.ex <- function()
    pe1$educ <- as.factor(pe1$educ)
    pe1$occ <- as.factor(pe1$occ)
    print(summary(lm(wageinc ~ .,data=pe1)))  # full data
-   # bhat for gender 8558.76, s.e. 708.75
    pe2 <- pe1[,c(1,2,3,4,6,5)]
    # simulate NAs, making high-wage Occ 102, 1042 more likely NA
    occ <- pe2$occ
@@ -220,11 +219,9 @@ lm.pa.ex <- function()
    pe102$occ <- occ
    # complete cases only
    print(summary(lm(wageinc ~ .,data=pe102)))  
-   # Gender bhat now only 8275.05, s.e. increasing to 749.25
    # polyanNA, no discretization
    pe3 <- polyanNA(pe102,yCol=6)
    print(summary(lm.pa(pe3)$lmout))
-   # bhat for gender 8556.45, s.e. 708.78, almost same as full dataset
 }
 
 ### ###########################  lm.marg  ####################################
