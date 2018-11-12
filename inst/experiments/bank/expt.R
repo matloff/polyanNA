6c2c85ccdf894c25aeb909d5d42d5949e802e351
Norm Matloff <matloff@cs.ucdavis.edu>
2018-11-11 22:00:05 -0800

    update comment

--- inst/experiments/bank/expt.R
+++ inst/experiments/bank/expt.R
@@ -15,7 +15,7 @@ bank <-
 bank$y <- as.integer(bank$y == 'yes')
 bank$job <- NULL  # had data issues
 bank$poutcome <- NULL  # almost all unknown
-bnk <- f()  # see f below; changes 'unknown' to NA
+bnk <- f()  
 bnk1 <- factorsToDummies(bnk)
 doGenExpt(bnk1,regftn=glm)
 
